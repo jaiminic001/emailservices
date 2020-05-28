b4c3b39d9a8779ed5145a6eaf413adbc0596f0a6
Pankaj <psharmam15@gmail.com>
2020-05-28 10:52:37 +0530

    edit

--- src/main/java/com/pixels/texchange/email/emailservices/EmailservicesApplication.java
+++ src/main/java/com/pixels/texchange/email/emailservices/EmailservicesApplication.java
@@ -1,15 +1,23 @@
 package com.pixels.texchange.email.emailservices;
 
 import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeBodyPart;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeMultipart;
 import com.sun.xml.internal.org.jvnet.mimepull.MIMEMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import sun.plugin2.message.Message;
+import sun.rmi.transport.Transport;
+import javax.mail.internet.Addressexception;
+import javax.mail.internet.InternetAddress;
+import javax.mail.Session;
 
 import javax.xml.bind.annotation.XmlEnumValue;
 import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.net.PasswordAuthentication;
 import java.util.Properties;
+
 
 @SpringBootApplication
 @RestController
@@ -25,12 +33,12 @@ public class EmailservicesApplication {
 
 
 	@RequestMapping(value="/send",method=RequestMethod.POST)
-	public String sendEmail(@RequestBody EmailMessage emailmessage) throws AddressException, MessagingException{
+	public String sendEmail(@RequestBody EmailMessage emailmessage) throws AddressException, MessagingException, IOException {
 		sendmail(emailmessage);
 		return "Email sent successfully";
 	}
 
-	private void sendmail(EmailMessage emailmessage) throws AddressException, MessagingException {
+	private void sendmail(EmailMessage emailmessage) throws AddressException, MessagingException,IOException{
 
 		Properties props = new Properties();
 		props.put("mail.smtp.auth","true");
@@ -50,6 +58,19 @@ public class EmailservicesApplication {
 		msg.setSubject(emailmessage.getSubject());
 		msg.setContent(emailmessage.getBody(),"text/html");
 		msg.setSentDate(new Data());
+
+		MimeBodyPart messageBodyPart = new MimeBodyPart();
+		messageBodyPart.setContent(emailmessage.getBody(),"text/html");
+
+		MultiPart multipart = new MimeMultipart();
+		multipart.addBodyPart(messageBodyPart);
+		MimeBodyPart attachPart = new MimeBodyPart();
+		attachPart.attachFile("");
+		multipart.addBodyPart(attachPart);
+		msg.setContent(multipart);
+		//send the email
+		Transport.send(msg);
+
 
 
 	}
